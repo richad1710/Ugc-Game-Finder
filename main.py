b57a6a17ef650c34ffeb553af6b834585ad4891d
richad1710 <104232479+richad1710@users.noreply.github.com>
2023-08-06 20:26:29 -0300

    Fixed 2 Universe Ids In Game Link And Added Auto Restart to main.py

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ try:
         import requests
         import os
         from rgbprint import gradient_print, color, Color
+        from time import sleep as wait
         import webbrowser
         import json
+        import keyboard
     except ModuleNotFoundError as errora:
         import os
         print(f'{errora} Installing Now')
-        os.system('pip install requests')
-        os.system('pip install rgbprint')
+        os.system('pip install requests rgbprint keyboard')
         quit()
 except ModuleNotFoundError as error:
     import os
@@ -21,7 +22,9 @@ except ModuleNotFoundError as error:
 j = open('config.json')
 data = json.load(j)
 
-logo = ("""
+def Finder():
+
+    logo = ("""
   d8,                                          ,d8888b  d8,                d8b                 
  `8P                                           88P'    `8P                 88P                 
                                             d888888P                      d88                  
@@ -32,52 +35,62 @@ d88' `?88P'`88b`?8888P'd88'     `?888P'     d88'      d88' d88'   88b`?88P'`88b`
             )88                                                                                
            ,88P                                                                                
        `?8888P                                                                                 
-""")
+    """)
 
-gradient_print(logo, start_color=(69, 140, 255), end_color=(135, 181, 255))
-gradient_print("What Is The Item Id?", start_color=(69, 140, 255), end_color=(135, 181, 255))
-ids = input(f"{Color((69, 140, 255))}>{Color.white} ")
+    gradient_print(logo, start_color=(69, 140, 255), end_color=(135, 181, 255))
+    gradient_print("What Is The Item Id?", start_color=(69, 140, 255), end_color=(135, 181, 255))
+    ids = input(f"{Color((69, 140, 255))}>{Color.white} ")
 
-session = requests.Session()
-session.cookies[".ROBLOSECURITY"] = (data)["account"]['cookie']
+    session = requests.Session()
+    session.cookies[".ROBLOSECURITY"] = data["account"]['cookie']
 
-def rbx_request(method, url, **kwargs):
-    request = session.request(method, url, **kwargs)
-    method = method.lower()
-    if (method == "post") or (method == "put") or (method == "patch") or (method == "delete"):
-        if "X-CSRF-TOKEN" in request.headers:
-            session.headers["X-CSRF-TOKEN"] = request.headers["X-CSRF-TOKEN"]
-            if request.status_code == 403:
-                request = session.request(method, url, **kwargs)
-    return request
+    def rbx_request(method, url, **kwargs):
+        request = session.request(method, url, **kwargs)
+        method = method.lower()
+        if (method == "post") or (method == "put") or (method == "patch") or (method == "delete"):
+            if "X-CSRF-TOKEN" in request.headers:
+                session.headers["X-CSRF-TOKEN"] = request.headers["X-CSRF-TOKEN"]
+                if request.status_code == 403:
+                    request = session.request(method, url, **kwargs)
+        return request
 
 
-req = rbx_request("GET", f"https://economy.roblox.com/v2/assets/{ids}/details")
+    req = rbx_request("GET", f"https://economy.roblox.com/v2/assets/{ids}/details")
 
-if req.status_code == 400:
-    print("Not a Valid Id")
-    os.system("pause")
-    quit()
+    if req.status_code == 400:
+        print("Not a Valid Id")
+        os.system("pause")
+        quit()
+    
+    if (req.json())["SaleLocation"] is None or len((req.json())["SaleLocation"].get("UniverseIds", [])) == 0:
+        print("Put Ur Roblox Cookie Or This Item Doesn't Have An Id")
+        os.system("pause")
+        quit()
 
-if (req.json())["SaleLocation"] is None or len((req.json())["SaleLocation"].get("UniverseIds", [])) == 0:
-    print("Put Ur Roblox Cookie Or This Item Doesn't Have An Id")
-    os.system("pause")
-    quit()
+    universe_ids = (req.json())['SaleLocation'].get('UniverseIds')[0]
+    if (req.json())['SaleLocation'].get('UniverseIds')[1] in req.json()['SaleLocation']['UniverseIds']:
+        universe_ids = (req.json())['SaleLocation'].get('UniverseIds')[1]
 
-universe_ids = (req.json())['SaleLocation'].get('UniverseIds', [])
+    req2 = rbx_request("GET", (f"https://games.roblox.com/v1/games?universeIds={universe_ids}"))
 
-req2 = rbx_request("GET", (f"https://games.roblox.com/v1/games?universeIds={','.join(map(str, universe_ids))}"))
+    game_list = []
+    for current in (req2.json())["data"]:
+        game_list.append(current['rootPlaceId'])
 
-game_list = []
-for current in (req2.json())["data"]:
-    game_list.append(current['rootPlaceId'])
+    req3 = rbx_request("GET", (f"https://www.roblox.com/games/{','.join(map(str, game_list))}"))
+    print(str(req3.url))
+    if data['game']['auto-join'] == True:
+        webbrowser.open(f'roblox://experiences/start?placeId={",".join(map(str, game_list))}')
+        print('Opened Browser')
+    else:
+        pass
+    universe_ids = []
+    game_list = []
 
-req3 = rbx_request("GET", (f"https://www.roblox.com/games/{','.join(map(str, game_list))}"))
-print(str(req3.url))
-if data['game']['auto-join'] == True:
-    webbrowser.open(f'roblox://experiences/start?placeId={",".join(map(str, game_list))}')
-    print('Opened Browser')
-else:
-    print('Auto-join Disabled')
-
-os.system("pause")
+Finder()
+os.system('pause')
+while True:
+    os.system("cls")
+    Finder()
+    os.system('pause')
+    wait(1)
